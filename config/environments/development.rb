348c33d5562bd6fa2f7d9750af145b42f8f39bbf
Edward Lando <edwardlando8@gmail.com>
2012-09-11 00:37:10 -0400

    added devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ LandoYoushaei::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # Added with devise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
