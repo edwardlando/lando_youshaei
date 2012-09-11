798245c9c3cf75da029155e6ebfedec1dce62318
Edward Lando <edwardlando8@gmail.com>
2012-09-11 00:08:39 -0400

    first commit:

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LandoYoushaei::Application.config.secret_token = '3c28e93f0cc336c8456d204c7fdae760488b04063dfd808d7411f0597382954a96469440a4c948fa19e113a435438be681c09ec29c1680ce1e721c93307582ff'
