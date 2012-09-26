28836f7163c3a3becfb3e73968b2ce422bd1fc34
Edward Lando <edwardlando8@gmail.com>
2012-09-26 12:33:13 -0400

    progress on switching channels but need to be able to switch back

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -6,11 +6,14 @@ class StoreController < ApplicationController
 
       # Determines which channel we're on
       if params[:switch_channel] == "true"
+        @old_current_channel = @user.current_channel
+        @old_current_channel.current_channel = false
         @channel = Channel.find_by_id(params[:channel_id])
+        @channel.current_channel = true
       else
          @channel = @user.current_channel
       end
-       
+      
       # Allows us to get the wanted item, thanks to its index
       @channel_items = @channel.channel_items 
       @channel.item_index = params[:index]
@@ -19,11 +22,12 @@ class StoreController < ApplicationController
         @item_url = @channel.current_item_url
       end
       @channel.save 
+      @user.save
     end
    
 
   respond_to do |format|
-      format.html # index.html.erb
+      format.html {}# index.html.erb
       format.json {}
   end
  end
