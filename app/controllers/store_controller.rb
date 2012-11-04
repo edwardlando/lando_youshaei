f9b95e84ac718dac35b06f94d1660cbe83dd6b9b
edwardlando <edwardlando8@gmail.com>
2012-11-04 14:33:48 -0500

    fixing channel bug

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -7,9 +7,7 @@ class StoreController < ApplicationController
       @orders = Order.all.sort_by(&:updated_at) # will want to sort by paid and unpaid
       @confirmations = Confirmation.all
 
-      @channel = Channel.find_by_current_channel(true)
-
-      # Handling orders and confirmations
+       # Handling orders and confirmations
       unless (@orders.nil? || @orders.empty? || @confirmations.nil? || @confirmations.empty? )
         @confirmation = Confirmation.find_by_user_id_and_status(@user.id, "sent_to_customer")  # might need an index
         @order = @confirmation.order unless @confirmation.nil?
@@ -19,15 +17,16 @@ class StoreController < ApplicationController
         end
       end 
 
-      # Determines which channel we're on
+       # Switching channels
       if params[:switch_channel] == "true"
-        @channel = Channel.find_by_id(params[:channel_id])
-        @channel.item_index = 0 #added this because of no method error
+        @channel = @channels.find(params[:channel_id])
+        @channel.item_index = 0 
         @channels.each do |channel|
           channel.current_channel = false
         end
         @channel.current_channel = true
-        @channel.save
+      else 
+        @channel = @channels.find_by_current_channel(true)
       end
 
       # Allows us to get the wanted item, thanks to its index
@@ -36,15 +35,14 @@ class StoreController < ApplicationController
       else
         @channel.item_index = 0
       end
-        @channel.save
-      
+       
       unless @channel.channel_items.empty? 
         @channel_items = @channel.channel_items 
         @current_item = @channel.channel_items[@channel.item_index]
         @item_url = @channel.current_item_url
       end
+
       @channel.save 
-      @user.save
     end
    
   respond_to do |format|
