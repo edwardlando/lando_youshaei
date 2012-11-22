8a654b82e762a2e514e795724c39261921b211d8
edwardlando <edwardlando8@gmail.com>
2012-11-22 02:56:52 -0500

    no more duplicate channels when loging in with twitter

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,15 +1,20 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 	def all
 	    user = User.from_omniauth(request.env["omniauth.auth"])
-	    user.role = "standard"
-	    user.cart = Cart.new(:user_id => user.id)
-	    user.wishlist = Wishlist.new(:user_id => user.id)
-	    
-	    # Initial channel for the user
-	    @channel = Channel.new(:color => "All", :style => "All", :price => "All", :gender => "Unisex",
-    :user_id => user.id, :item_index => 0, :name => "My first channel")
-	    @channel.current_channel = true
-	    @channel.save
+
+        # not making duplicates
+	    if user.channels.empty? && user.cart.nil? && user.wishlist.nil?
+		    user.role = "standard"
+		    user.cart = Cart.new(:user_id => user.id)
+		    user.wishlist = Wishlist.new(:user_id => user.id)
+		    
+		    # Initial channel for the user
+		    @channel = Channel.new(:color => "All", :style => "All", :price => "All", :gender => "Unisex",
+	    :user_id => user.id, :item_index => 0, :name => "My first channel")
+		    @channel.current_channel = true
+		    @channel.save
+	    end
+
 	    if user.persisted?
 	      flash.notice = "Signed in!"
 	      sign_in_and_redirect user
