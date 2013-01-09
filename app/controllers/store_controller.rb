b31fbd3794662b33f1877ccef7e441375a76f6ff
edwardlando <edwardlando8@gmail.com>
2013-01-09 14:16:20 -0500

    started changing header color based on new design

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -5,29 +5,14 @@ class StoreController < ApplicationController
     if current_or_guest_user #should check if a current or guest user exists
     	@user = current_user
       @guest_user = guest_user
-      @channels = @user.channels unless @user.nil?
-      #@items = Item.page(params[:page]).per(5)   ##### starting kaminari change here
       @index = params["index"].to_i ||= 0
-      ### need to add params page
-=begin
-      @orders = Order.all.sort_by(&:updated_at) # will want to sort by paid and unpaid
-      @confirmations = Confirmation.all
 
-      # Handling orders and confirmations
-      unless (@user.nil? || @orders.nil? || @orders.empty? || @confirmations.nil? || @confirmations.empty? )
-        @confirmation = Confirmation.find_by_user_id_and_status(@user.id, "sent_to_customer")  # might need an index
-        @order = @confirmation.order unless @confirmation.nil?
-        if params[:confirmation_status]
-          @confirmation.status = params[:confirmation_status]
-          @confirmation.save
-        end
-      end 
-=end
       if @user
         if params[:current_channel] && params[:switch]
            @old_channel = @user.current_channel
            @old_channel.current_channel = false
            @old_channel.save
+           @channels = @user.channels
            @channel = @channels.find(params[:current_channel])
            @channel.current_channel = true
            @channel.item_index = 0
@@ -38,9 +23,7 @@ class StoreController < ApplicationController
       elsif @guest_user
         @guest_channel = @guest_user.current_channel
       end
-
-      @channel = @user.current_channel unless @user.nil?
-
+    
       if @channel 
         @items = @channel.channel_items # send index in json too let's see if just sending the url works
         @items = @items[@index..@index+2]
@@ -55,23 +38,29 @@ class StoreController < ApplicationController
 
       @item_urls = []
         unless @items.nil?
-        @items.each do |item|
-          @item_urls << item.url
-        end
-      end
-
-      
+          @items.each do |item|
+            @item_urls << item.url
+          end
+        end    
     end
     respond_to do |format|
-        format.html { } # index.html.erb
-        #format.js 
+        format.html # index.html.erb
         format.json { render :json => @item_urls } # send index too
-
-        #somewhere else, call items.paginate (will only give you a certain amount of items, say 5)
-        #render json items
-        #allows you to request the next page
-
-
     end
   end
 end
+
+=begin
+      @orders = Order.all.sort_by(&:updated_at) # will want to sort by paid and unpaid
+      @confirmations = Confirmation.all
+
+      # Handling orders and confirmations
+      unless (@user.nil? || @orders.nil? || @orders.empty? || @confirmations.nil? || @confirmations.empty? )
+        @confirmation = Confirmation.find_by_user_id_and_status(@user.id, "sent_to_customer")  # might need an index
+        @order = @confirmation.order unless @confirmation.nil?
+        if params[:confirmation_status]
+          @confirmation.status = params[:confirmation_status]
+          @confirmation.save
+        end
+      end 
+=end
